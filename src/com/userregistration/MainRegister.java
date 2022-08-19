65bf8a80b6a2d075a347cbe98b655e4577898aef
Kritika <kritistarsmind@gmail.com>
2022-08-19 20:01:28 +0530

    user registration

---
+++ src/com/userregistration/MainRegister.java
@@ -0,0 +1,22 @@
+package com.userregistration;
+import java.util.*;
+public class MainRegister {
+	public static void main(String args[]){
+		MainRegister register=new MainRegister();
+		
+		try(Scanner scanner=new Scanner(System.in)){
+
+		System.out.println("Enter first name");
+		String firstName=scanner.nextLine();
+		System.out.println("Enter last name");
+		String lastName=scanner.nextLine();
+		System.out.println("Enter email id");
+		String emailId=scanner.nextLine();
+		System.out.println("Enter password");
+		String password=scanner.nextLine();
+		System.out.println("Phone number is");
+		long phoneNo=scanner.nextInt();
+
+}
+	}
+}
---
+++ src/com/userregistration/UserRegistration.java
@@ -0,0 +1,42 @@
+package com.userregistration;
+
+public class UserRegistration {
+	private String firstName;
+	private String lastName;
+	private String emailId;
+	private String password;
+	private String phoneNo;
+	
+		public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+		
+
+}
